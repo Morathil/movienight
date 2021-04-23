6a6d6eac2c3f7905589ac51a6fbf0c0410bdc53d
golali <a.golchini@outlook.de>
2021-04-23 09:27:13 +0200

    delete unused imports

--- src/views/pages/GroupDetails.jsx
+++ src/views/pages/GroupDetails.jsx
@@ -1,13 +1,23 @@
-import { connect } from 'react-redux'
-import React, { Component } from 'react'
-import { Grid, Typography, Button, List, ListItem, ListItemText, ListItemSecondaryAction } from '@material-ui/core/'
+import {
+  connect
+} from 'react-redux'
+import React, {
+  Component
+} from 'react'
+import {
+  Grid,
+  Typography,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction
+} from '@material-ui/core/'
 import * as uiActions from 'actions/ui'
 import BottomButton from 'views/components/BottomButton'
 import TopBar from 'views/components/TopBar'
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
-import ListSubheader from '@material-ui/core/ListSubheader';
-
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -15,95 +25,142 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-import Box from '@material-ui/core/Box';
-import { spacing } from '@material-ui/system';
-
-
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
 class GroupDetails extends Component {
-  render () {
-    const { group, topRatedMovies } = this.props;
-    return (
-      <Grid container style={{ minHeight: '100vh' }} direction='column'>
-        <TopBar />
-        <Grid item xs={12}>
-          <Typography variant='h3'>
-            {group.name} - {new Date(group.dateTime).toLocaleDateString()}
-          </Typography>
-        </Grid>
-        <Grid item style={{ minHeight: '10vh' }} xs={12}>
-        {group.description}
-        </Grid>
-        <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Movie Title</TableCell>
-                <TableCell align="right">Rating</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {topRatedMovies.map((movie) => {
-                return (
-                <TableRow key={movie.id}>
-                  <TableCell component="th" scope="row">
-                    {movie.title}
-                  </TableCell>
-                  <TableCell>{movie.memberRatings ? (Object.values(movie.memberRatings).reduce((value, reduction) => value + reduction, 0) / Object.values(movie.memberRatings).length) : 0}
-                  </TableCell>
-                </TableRow>
-              )
-              })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Grid item xs={12}>
-          <Typography variant='body1'>
-            Share the token with your friends: {group.token}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant='body1'>
-            Members:
-          </Typography>
-          <List>
-            {Object.values(group.nameByMemberId).map((name, i) => {
-              return (
-                <ListItem key={i}>
-                  <ListItemText primary={name} />
-                </ListItem>
-              )
-            })}
-          </List>
-        </Grid>
-        <Grid item xs={12}>
-          <BottomButton onAction={this.handleOnRateMovie}>
-            Rate Movies
-          </BottomButton>
-        </Grid>
-      </Grid>
+  render() {
+    const {
+      group,
+      topRatedMovies
+    } = this.props;
+    return ( <
+      Grid container style = {
+        {
+          minHeight: '100vh'
+        }
+      }
+      direction = 'column' >
+      <
+      TopBar / >
+      <
+      Grid item xs = {
+        12
+      } >
+      <
+      Typography variant = 'h3' > {
+        group.name
+      } - {
+        new Date(group.dateTime).toLocaleDateString()
+      } <
+      /Typography> <
+      /Grid> <
+      Grid item style = {
+        {
+          minHeight: '10vh'
+        }
+      }
+      xs = {
+        12
+      } > {
+        group.description
+      } <
+      /Grid> <
+      TableContainer component = {
+        Paper
+      } >
+      <
+      Table aria - label = "simple table" >
+      <
+      TableHead >
+      <
+      TableRow >
+      <
+      TableCell > Movie Title < /TableCell> <
+      TableCell align = "right" > Rating < /TableCell> <
+      /TableRow> <
+      /TableHead> <
+      TableBody > {
+        topRatedMovies.map((movie) => {
+          return ( <
+            TableRow key = {
+              movie.id
+            } >
+            <
+            TableCell component = "th"
+            scope = "row" > {
+              movie.title
+            } <
+            /TableCell> <
+            TableCell > {
+              movie.memberRatings ? (Object.values(movie.memberRatings).reduce((value, reduction) => value + reduction, 0) / Object.values(movie.memberRatings).length) : 0
+            } <
+            /TableCell> <
+            /TableRow>
+          )
+        })
+      } <
+      /TableBody> <
+      /Table> <
+      /TableContainer> <
+      Grid item xs = {
+        12
+      } >
+      <
+      Typography variant = 'body1' >
+      Share the token with your friends: {
+        group.token
+      } <
+      /Typography> <
+      /Grid> <
+      Grid item xs = {
+        12
+      } >
+      <
+      Typography variant = 'body1' >
+      Members:
+      <
+      /Typography> <
+      List > {
+        Object.values(group.nameByMemberId).map((name, i) => {
+          return ( <
+            ListItem key = {
+              i
+            } >
+            <
+            ListItemText primary = {
+              name
+            }
+            /> <
+            /ListItem>
+          )
+        })
+      } <
+      /List> <
+      /Grid> <
+      Grid item xs = {
+        12
+      } >
+      <
+      BottomButton onAction = {
+        this.handleOnRateMovie
+      } >
+      Rate Movies <
+      /BottomButton> <
+      /Grid> <
+      /Grid>
     );
   }
 
   handleOnRateMovie = () => {
-    const { dispatch, group } = this.props
-    dispatch(uiActions.changePage('RateMovies', { groupId: group.id }))
+    const {
+      dispatch,
+      group
+    } = this.props
+    dispatch(uiActions.changePage('RateMovies', {
+      groupId: group.id
+    }))
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   const groupId = state.ui.page.data.groupId
   const group = state.groups.entities[groupId]
   const topRatedMovies = Object.values(group.movies).sort((ma, mb) => {
